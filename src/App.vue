89ee8bcb1e948831e5e54c24b92751fd2244fe44
Roman Matsypula <ukrmatsypula@gmail.com>
2023-07-03 00:45:07 +0300

    add favicon, created components from youtube landing

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <TheHeader/>
+  <TheSidebarSmall/>
+  <TheSidebar/>
+  <TheSidebarMobile/>
+  <TheCategories/>
+  <TheVideos/>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import TheSidebarSmall from "./components/TheSidebarSmall.vue";
+import TheSidebar from "./components/TheSidebar.vue";
+import TheSidebarMobile from "./components/TheSidebarMobile.vue";
+import TheCategories from "./components/TheCategories.vue";
+import TheVideos from "./components/TheVideos.vue";
+
+export default {
+  components: {TheHeader, TheSidebarSmall, TheSidebar, TheSidebarMobile, TheCategories, TheVideos}
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
+</script>
---
+++ src/components/TheSidebarSmall.vue
@@ -0,0 +1,129 @@
+<template>
+  <aside class="xl:hidden hidden md:block min-h-screen overflow-auto fixed top-0 pt-14 z-20 bg-white">
+    <section>
+      <ul>
+        <li>
+          <a
+              href="#"
+              class="flex flex-col items-center px-2 py-5 text-red-500 hover:bg-gray-100"
+          >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                class="w-6 h-6 mb-1.5"
+            >
+              <path
+                  fill-rule="evenodd"
+                  d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z"
+                  clip-rule="evenodd"
+              />
+            </svg>
+
+            <span class="text-xs">Home</span>
+          </a>
+        </li>
+
+        <li>
+          <a
+              href="#"
+              class="flex flex-col items-center px-2 py-5 hover:bg-gray-100"
+          >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="w-6 h-6 mb-1.5"
+            >
+              <path
+                  fill-rule="evenodd"
+                  d="M12.963 2.286a.75.75 0 00-1.071-.136 9.742 9.742 0 00-3.539 6.177A7.547 7.547 0 016.648 6.61a.75.75 0 00-1.152-.082A9 9 0 1015.68 4.534a7.46 7.46 0 01-2.717-2.248zM15.75 14.25a3.75 3.75 0 11-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 011.925-3.545 3.75 3.75 0 013.255 3.717z"
+                  clip-rule="evenodd"
+              />
+            </svg>
+
+            <span class="text-xs">Trending</span>
+          </a>
+        </li>
+
+        <li>
+          <a
+              href="#"
+              class="flex flex-col items-center px-2 py-5 hover:bg-gray-100"
+          >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="w-6 h-6 mb-1.5"
+            >
+              <path
+                  d="M21 6.375c0 2.692-4.03 4.875-9 4.875S3 9.067 3 6.375 7.03 1.5 12 1.5s9 2.183 9 4.875z"
+              />
+              <path
+                  d="M12 12.75c2.685 0 5.19-.586 7.078-1.609a8.283 8.283 0 001.897-1.384c.016.121.025.244.025.368C21 12.817 16.97 15 12 15s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.285 8.285 0 001.897 1.384C6.809 12.164 9.315 12.75 12 12.75z"
+              />
+              <path
+                  d="M12 16.5c2.685 0 5.19-.586 7.078-1.609a8.282 8.282 0 001.897-1.384c.016.121.025.244.025.368 0 2.692-4.03 4.875-9 4.875s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.284 8.284 0 001.897 1.384C6.809 15.914 9.315 16.5 12 16.5z"
+              />
+              <path
+                  d="M12 20.25c2.685 0 5.19-.586 7.078-1.609a8.282 8.282 0 001.897-1.384c.016.121.025.244.025.368 0 2.692-4.03 4.875-9 4.875s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.284 8.284 0 001.897 1.384C6.809 19.664 9.315 20.25 12 20.25z"
+              />
+            </svg>
+
+            <span class="text-xs">Subscriptions</span>
+          </a>
+        </li>
+
+        <li>
+          <a
+              href="#"
+              class="flex flex-col items-center px-2 py-5 hover:bg-gray-100"
+          >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="w-6 h-6 mb-1.5"
+            >
+              <path
+                  fill-rule="evenodd"
+                  d="M7.502 6h7.128A3.375 3.375 0 0118 9.375v9.375a3 3 0 003-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 00-.673-.05A3 3 0 0015 1.5h-1.5a3 3 0 00-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6zM13.5 3A1.5 1.5 0 0012 4.5h4.5A1.5 1.5 0 0015 3h-1.5z"
+                  clip-rule="evenodd"
+              />
+              <path
+                  fill-rule="evenodd"
+                  d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V9.375zM6 12a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V12zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75zM6 15a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V15zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75zM6 18a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V18zm2.25 0a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H9a.75.75 0 01-.75-.75z"
+                  clip-rule="evenodd"
+              />
+            </svg>
+
+            <span class="text-xs">Library</span>
+          </a>
+        </li>
+
+        <li>
+          <a
+              href="#"
+              class="flex flex-col items-center px-2 py-5 text-red-500 hover:bg-gray-100"
+          >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="w-6 h-6 mb-1.5"
+            >
+              <path
+                  fill-rule="evenodd"
+                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z"
+                  clip-rule="evenodd"
+              />
+            </svg>
+
+            <span class="text-xs">History</span>
+          </a>
+        </li>
+      </ul>
+    </section>
+  </aside>
+</template>
