8ca1479489865eb6a33b02a57872273ba803c83b
Roman Matsypula <ukrmatsypula@gmail.com>
2023-07-04 23:02:51 +0300

    align code with prettier

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <TheHeader/>
-  <TheSidebarSmall/>
-  <TheSidebar/>
-  <TheSidebarMobile/>
-  <TheCategories/>
-  <TheVideos/>
+  <TheHeader />
+  <TheSidebarSmall />
+  <TheSidebar />
+  <TheSidebarMobile />
+  <TheCategories />
+  <TheVideos />
 </template>
 
 <script>
@@ -16,7 +16,13 @@ import TheCategories from "./components/TheCategories.vue";
 import TheVideos from "./components/TheVideos.vue";
 
 export default {
-  components: {TheHeader, TheSidebarSmall, TheSidebar, TheSidebarMobile, TheCategories, TheVideos}
-}
-
+  components: {
+    TheHeader,
+    TheSidebarSmall,
+    TheSidebar,
+    TheSidebarMobile,
+    TheCategories,
+    TheVideos,
+  },
+};
 </script>
--- src/components/TheDropdownApps.vue
+++ src/components/TheDropdownApps.vue
@@ -1,127 +1,127 @@
 <template>
   <div
-            class="opacity-0 group-hover:opacity-100 absolute top-9 bg-white w-60 border border-t-0 right-0 sm:left-0"
-        >
-          <section class="py-2 border-b">
-            <ul>
-              <li>
-                <a
-                    href="#"
-                    class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
-                >
-                  <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      class="w-6 h-6 mr-3 text-red-500"
-                  >
-                    <path
-                        fill-rule="evenodd"
-                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
-                        clip-rule="evenodd"
-                    />
-                  </svg>
+    class="opacity-0 group-hover:opacity-100 absolute top-9 bg-white w-60 border border-t-0 right-0 sm:left-0"
+  >
+    <section class="py-2 border-b">
+      <ul>
+        <li>
+          <a
+            href="#"
+            class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-6 h-6 mr-3 text-red-500"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
+                clip-rule="evenodd"
+              />
+            </svg>
 
-                  <span>YouTube TV</span>
-                </a>
-              </li>
-            </ul>
-          </section>
+            <span>YouTube TV</span>
+          </a>
+        </li>
+      </ul>
+    </section>
 
-          <section class="py-2 border-b">
-            <ul>
-              <li>
-                <a
-                    href="#"
-                    class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
-                >
-                  <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      class="w-6 h-6 mr-3 text-red-500"
-                  >
-                    <path
-                        fill-rule="evenodd"
-                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
-                        clip-rule="evenodd"
-                    />
-                  </svg>
+    <section class="py-2 border-b">
+      <ul>
+        <li>
+          <a
+            href="#"
+            class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-6 h-6 mr-3 text-red-500"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
+                clip-rule="evenodd"
+              />
+            </svg>
 
-                  <span>YouTube Music</span>
-                </a>
-              </li>
+            <span>YouTube Music</span>
+          </a>
+        </li>
 
-              <li>
-                <a
-                    href="#"
-                    class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
-                >
-                  <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      class="w-6 h-6 mr-3 text-red-500"
-                  >
-                    <path
-                        fill-rule="evenodd"
-                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
-                        clip-rule="evenodd"
-                    />
-                  </svg>
+        <li>
+          <a
+            href="#"
+            class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-6 h-6 mr-3 text-red-500"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
+                clip-rule="evenodd"
+              />
+            </svg>
 
-                  <span>YouTube Kids</span>
-                </a>
-              </li>
-            </ul>
-          </section>
+            <span>YouTube Kids</span>
+          </a>
+        </li>
+      </ul>
+    </section>
 
-          <section class="py-2">
-            <ul>
-              <li>
-                <a
-                    href="#"
-                    class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
-                >
-                  <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      class="w-6 h-6 mr-3 text-red-500"
-                  >
-                    <path
-                        fill-rule="evenodd"
-                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
-                        clip-rule="evenodd"
-                    />
-                  </svg>
+    <section class="py-2">
+      <ul>
+        <li>
+          <a
+            href="#"
+            class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-6 h-6 mr-3 text-red-500"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
+                clip-rule="evenodd"
+              />
+            </svg>
 
-                  <span>Creator Academy</span>
-                </a>
-              </li>
+            <span>Creator Academy</span>
+          </a>
+        </li>
 
-              <li>
-                <a
-                    href="#"
-                    class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
-                >
-                  <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      class="w-6 h-6 mr-3 text-red-500"
-                  >
-                    <path
-                        fill-rule="evenodd"
-                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
-                        clip-rule="evenodd"
-                    />
-                  </svg>
+        <li>
+          <a
+            href="#"
+            class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-6 h-6 mr-3 text-red-500"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
+                clip-rule="evenodd"
+              />
+            </svg>
 
-                  <span>YouTube for Artists</span>
-                </a>
-              </li>
-            </ul>
-          </section>
-        </div>
+            <span>YouTube for Artists</span>
+          </a>
+        </li>
+      </ul>
+    </section>
+  </div>
 </template>
