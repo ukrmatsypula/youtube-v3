89cee6dfc928583a2f104e5740bf359a0acc279f
Roman Matsypula <ukrmatsypula@gmail.com>
2023-07-04 23:09:31 +0300

    render active categories

--- src/components/TheCategories.vue
+++ src/components/TheCategories.vue
@@ -5,96 +5,40 @@
     <div class="border-t border-b px-4 max-w-screen-2xl mx-auto">
       <div class="py-3 flex space-x-3 overflow-auto text-sm whitespace-nowrap">
         <a
+          v-for="(categoty, index) in categories"
+          :key="index"
           href="#"
           class="px-3 py-1 transition bg-gray-600 border border-gray-700 rounded-full hover:bg-gray-500 text-white"
-          >All</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Trucks</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Tools</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Machines</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Engines</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Show</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Ships</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Roads</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Tanks</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Buildings</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Mars</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Woodworking</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Football</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >TV Shows</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Songs</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Tractors</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Woodworking</a
-        >
-        <a
-          href="#"
-          class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-          >Woodworking</a
+          >{{ categoty }}</a
         >
       </div>
     </div>
   </section>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    categories: [
+      "All",
+      "Trucks",
+      "Tools",
+      "Machines",
+      "Engines",
+      "Show",
+      "Ships",
+      "Roads",
+      "Tanks",
+      "Buildings",
+      "Mars",
+      "Woodworking",
+      "Football",
+      "TV Shows",
+      "Songs",
+      "Tractors",
+      "Woodworking",
+      "Woodworking",
+    ],
+  }),
+};
+</script>
