45c1bcdcf25b539ef9811051563b8d4183de8183
Roman Matsypula <ukrmatsypula@gmail.com>
2023-07-05 14:45:27 +0300

    add search input with buttons

---
+++ src/components/TheSearchButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button
+    class="flex items-center px-6 py-1 text-gray-600 bg-gray-100 border border-left-0 border-gray-300 rounded-tr-sm rounded-br-sm hover:bg-gray-200"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="w-5 h-5"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  </button>
+</template>
---
+++ src/components/TheSearchInput.vue
@@ -0,0 +1,7 @@
+<template>
+  <input
+    type="text"
+    class="border border-gray-300 w-full px-3 shadow-inner rounded-bl rounded-tl focus:outline-none focus:border-blue-700"
+    placeholder="Search"
+  />
+</template>
