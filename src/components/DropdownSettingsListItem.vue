a0c00228137bd85cd67239b459dde0493268b955
Roman Matsypula <ukrmatsypula@gmail.com>
2023-07-11 23:30:23 +0300

    add component DropdownSettingsListItem

---
+++ src/components/DropdownSettingsListItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <a href="#" class="flex items-center px-4 py-2 text-sm hover:bg-gray-100">
+      <BaseIcon v-if="icon" :name="icon" class="w-6 h-6 mr-3 text-gray-400" />
+
+      <span>{{ label }}</span>
+      <BaseIcon
+        v-if="withSubMenu"
+        name="chevronRight"
+        class="w-4 h-4 mr-3 text-gray-400 ml-auto"
+      />
+    </a>
+  </li>
+</template>
+
+<script>
+import BaseIcon from "./BaseIcon.vue";
+
+export default {
+  components: {
+    BaseIcon,
+  },
+
+  props: {
+    label: String,
+    icon: String,
+    withSubMenu: Boolean,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
